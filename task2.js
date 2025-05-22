4e44bd62eaa637b2803080bbdf7193871a0e83e3
Demm1le <Halikovadel26@gmail.com>
2025-05-22 13:23:31 +0300

    Create task2.js

---
+++ task2.js
@@ -0,0 +1,20 @@
+# 3.1 Вывод примера 2+2
+print 2 + 2
+
+# 3.2 Приветствие с возрастом
+let age = 25
+print "Привет! Мне " + age
+
+# 3.3 2 + 2 = 4 с переменными
+let a = 2
+let b = 2
+let c = a + b
+print a
+print "+"
+print b
+print "="
+print c
+
+# 3.4 Уравнение x = 2 + 2 * 2
+let x = 2 + 2 * 2
+print x
